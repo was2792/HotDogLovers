e20104bb1828012f18045b2f7eaae9c49ad86686
was2792 <was.shelton@gmail.com>
2015-03-25 15:49:28 -0400

    Css Changes to Details
    Changed layout things

--- HotDog/HotDog/Models/Profile.cs
+++ HotDog/HotDog/Models/Profile.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
 
 namespace HotDog.Models
 {
@@ -9,14 +13,19 @@ namespace HotDog.Models
     {
         public int ProfileID { get; set; }
 
+        [Display(Name="Full Name")]
         public string Name { get; set; }
 
+        [Display(Name="Favorite Hot Dog")]
         public string Favorite { get; set; }
 
+        [Display(Name="Location of Last Dog Eaten")]
         public string Location { get; set; }
 
+        [Display(Name="Personal Hot Dog Facts")]
         public string Bio { get; set; }
 
+       [Display(Name="Most Recent Dog Eaten")]
         public string Dog { get; set; }
     }
 }
