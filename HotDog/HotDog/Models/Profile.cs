60fe02b97d3af2f3460aa2057ad6817ffec838cb
was2792 <was.shelton@gmail.com>
2015-03-25 16:05:42 -0400

    Minor Changes to Labels
    changes to labels

--- HotDog/HotDog/Models/Profile.cs
+++ HotDog/HotDog/Models/Profile.cs
@@ -13,19 +13,19 @@ namespace HotDog.Models
     {
         public int ProfileID { get; set; }
 
-        [Display(Name="Full Name")]
+        
         public string Name { get; set; }
 
-        [Display(Name="Favorite Hot Dog")]
+       
         public string Favorite { get; set; }
 
-        [Display(Name="Location of Last Dog Eaten")]
+        
         public string Location { get; set; }
 
-        [Display(Name="Personal Hot Dog Facts")]
+       
         public string Bio { get; set; }
 
-       [Display(Name="Most Recent Dog Eaten")]
+       
         public string Dog { get; set; }
     }
 }
