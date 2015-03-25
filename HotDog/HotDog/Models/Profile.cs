2d0ee8cda337a14d169a3ff536500fd1ac0b5786
was2792 <was.shelton@gmail.com>
2015-03-25 14:48:46 -0400

    Initial Hot Dog commit
    Initial Commit

---
+++ HotDog/HotDog/Models/Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotDog.Models
+{
+    public class Profile
+    {
+        public int ProfileID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Favorite { get; set; }
+
+        public string Location { get; set; }
+
+        public string Bio { get; set; }
+
+        public string Dog { get; set; }
+    }
+}
---
+++ HotDog/HotDog/Models/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotDog.Models
+{
+    public class ProfileViewModel
+    {
+        public List<String> Dogs { get; set; }
+        public List<String> Locs { get; set; }
+        public Profile Profile { get; set; }
+    }
+}
